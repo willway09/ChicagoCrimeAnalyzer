1a8981d337b662e220a47784a71d2633184139b5
willway09 <willway@outlook.com>
2022-04-13 16:42:51 -0400

    Query 1 should work 100%

--- website/JS/functions.js
+++ website/JS/functions.js
@@ -28,3 +28,97 @@ function stringToMonth(str) {
 	}
 	return month;
 }
+
+let months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December",
+];
+
+function clearChart(chart) {
+	chart.data.datasets = [];
+	chart.options.scales.y.title = {
+		display: false,
+	};
+	chart.update();
+}
+
+function setAxes(chart, xtitle, ytitle) {
+	chart.options.scales.x.title = {
+		display: true,
+		text: xtitle,
+	};
+
+	chart.options.scales.y.title = {
+		display: true,
+		text: ytitle,
+	};
+	chart.update();
+}
+
+function addDataSeries(chart, output, property, label) {
+	let data = [];
+	let labels = [];
+
+	output.forEach((x) => {
+		let point = {};
+		point.x = x.Year * 12 + x.Month;
+		point.y = x[property];
+		data.push(point);
+		labels.push(`${months[x.Month]} ${x.Year}`);
+	});
+
+	chart.data = {
+		labels: labels,
+		datasets: [
+			{
+				label: label,
+				pointRadius: 4,
+				pointBackgroundColor: "rgba(173,216,230,1)",
+				borderColor: "rgba(95,119,127,1)",
+				data: data,
+			},
+		],
+	};
+
+	chart.update();
+}
+
+
+function getDefaultChartConfig() {
+	return {
+		type: "line",
+		data: {
+			datasets: [{}],
+		},
+		options: {
+			plugins: {
+				zoom: {
+					pan: {
+						enabled: true,
+						mode: "x",
+					},
+					zoom: {
+						wheel: {
+							enabled: true,
+						},
+						pinch: {
+							enabled: true,
+						},
+						mode: "x",
+					},
+				},
+			},
+		},
+
+	}
+}
